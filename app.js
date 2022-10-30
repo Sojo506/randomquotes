ba16904acde69fa4817ba4c0e98c845bf56a5960
Sojo506 <fabiansojowork@gmail.com>
2022-10-30 15:41:45 -0600

    Removing api and adding a json

--- app.js
+++ app.js
@@ -1,36 +1,39 @@
-const text = document.querySelector('#text');
-const author = document.querySelector('#author');
-const btnQuote = document.querySelector('#new-quote');
-const quoteBox = document.querySelector('#quote-box');
-const tweetQuote = document.querySelector('#tweet-quote');
-const bird = document.querySelector('.fa-twitter')
+import {json} from "./quotes.js";
 
-document.addEventListener('DOMContentLoaded', () => {
+const text = document.querySelector("#text");
+const author = document.querySelector("#author");
+const btnQuote = document.querySelector("#new-quote");
+const quoteBox = document.querySelector("#quote-box");
+const tweetQuote = document.querySelector("#tweet-quote");
+const bird = document.querySelector(".fa-twitter");
+
+document.addEventListener("DOMContentLoaded", () => {
   generateQuote();
   generateColor();
-})
+});
 
-btnQuote.addEventListener('click', generateQuote)
+btnQuote.addEventListener("click", generateQuote);
 
 function generateQuote() {
-  fetch('http://quotable.io/random')
-  .then(response => response.json())
-  .then(response => {
-    text.textContent = response.content
-    author.textContent = response.author
-    tweetQuote.setAttribute('href', `https://twitter.com/intent/tweet?hashtags=quotes&related=Sojo5606&text="${response.content}" ${response.author}`)
-    
-    generateColor();
-  })
+  const quote = json[Math.floor(Math.random() * json.length)];
+
+  text.textContent = quote[0];
+  author.textContent = quote[1];
+  tweetQuote.setAttribute(
+    "href",
+    `https://twitter.com/intent/tweet?hashtags=quotes&related=Sojo5606&text="${quote[0]}" ${quote[1]}`
+  );
+
+  generateColor();
 }
 
 function generateColor() {
   const hex = "0123456789ABCDEF";
-  let aux = "#"
-  for(let i=0; i<6; i++) {
+  let aux = "#";
+  for (let i = 0; i < 6; i++) {
     aux += hex[Math.floor(Math.random() * 16)];
   }
-  quoteBox.style.color = aux
+  quoteBox.style.color = aux;
   document.body.style.backgroundColor = aux;
   tweetQuote.style.backgroundColor = aux;
 }
